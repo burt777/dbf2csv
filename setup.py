7f3198348911364d4ed34ed6379be3e81c6299b7
burt777 <burt777@gmail.com>
2016-03-03 12:26:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
-from distutils.core import setup
-import py2exe
+'''
 
-setup(console=['dbf2csv.py'])
+Compile dbf2csv into a single executable file,
+simply run:
+    python setup.py
+
+'''
+
+from distutils.core import setup
+import py2exe, sys, os
+
+# Show current python version
+print (sys.version)
+
+sys.argv.append('py2exe')
+
+setup(
+    console=['dbf2csv.py'],
+    options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
+    zipfile = None,
+)
