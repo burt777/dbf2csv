2f2e31c8eefb883a453d78e9521b597f34b2c08d
burt <burt777@gmail.com>
2016-03-03 12:29:08 +0100

    compile script

---
+++ compile.py
@@ -0,0 +1,21 @@
+'''
+
+Compile dbf2csv into a single executable file,
+simply run:
+    python compile.py
+
+'''
+
+from distutils.core import setup
+import py2exe, sys, os
+
+# Show current python version
+print (sys.version)
+
+sys.argv.append('py2exe')
+
+setup(
+    console=['dbf2csv.py'],
+    options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
+    zipfile = None,
+)
